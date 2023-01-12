4addccc742159c7f95928d1bcaa30295b9397e4a
Cagnard <oneman.cagnard.info@gmail.com>
2023-01-12 01:36:41 +0100

    Test Unit Sans Bug

--- test_unit.py
+++ test_unit.py
@@ -15,6 +15,6 @@ class MyTest(aiounittest.AsyncTestCase):
 
     def test_something(self):
         CONFIG = DefaultConfig()
-        LUIS_APP_ID = "1b4130ff-d5ba-4c40-a183-fd9e76d6f993000"
+        LUIS_APP_ID = "1b4130ff-d5ba-4c40-a183-fd9e76d6f993"
         luis_application = LuisApplication( LUIS_APP_ID, CONFIG.LUIS_API_KEY, "https://" + CONFIG.LUIS_API_HOST_NAME,)
         self.assertTrue(True)
